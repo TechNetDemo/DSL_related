71543dd9ae596eaabb9c195199aa8acf4f311a93
humphrey.lui <hum3lui@gmail.com>
2018-03-29 12:09:44 +0800

    added deploy pipeline

---
+++ deployWebAppPipeline.groovy
@@ -0,0 +1,18 @@
+node(node_to_run) {
+    def server = Artifactory.server 'AWS-Artifactory'
+    def downloadSpec = """{
+        "files": [
+            {
+              "pattern": "libs-snapshot-local/com/technet/webapp-team1/${artifact_version}/*.war",
+              "target": "ArtifactDirectory/"
+            }
+        ]
+    }"""
+        server.download(downloadSpec);
+        build job: '/SampleWebApp/deploy_webapp', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'db_url', value: db_url), string(name: 'db_tableName', value: db_tableName), string(name: 'artifact_version', value: artifact_version),[$class: 'NodeParameterValue', name: 'node_to_run', allNodesMatchingLabel: true,labels: [node_to_run], nodeEligibility: [$class: 'AllNodeEligibility'], triggerIfResult: 'allCases']]
+        s
+}
+post
+node(node_to_run){
+    echo 'influxDB task applys here'
+}
