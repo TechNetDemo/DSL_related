b307319126de48bc639a9c9044deb36a2450cec3
humphrey.lui <hum3lui@gmail.com>
2018-03-29 12:17:00 +0800

    pipeline point to job1

--- deployWebAppPipeline.groovy
+++ deployWebAppPipeline.groovy
@@ -9,7 +9,7 @@ node(node_to_run) {
         ]
     }"""
         server.download(downloadSpec);
-        build job: '/SampleWebApp/deploy_webapp', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'db_url', value: db_url), string(name: 'db_tableName', value: db_tableName), string(name: 'artifact_version', value: artifact_version),[$class: 'NodeParameterValue', name: 'node_to_run', allNodesMatchingLabel: true,labels: [node_to_run], nodeEligibility: [$class: 'AllNodeEligibility'], triggerIfResult: 'allCases']]
+        build job: '/DSLteam1/DEV/job1', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'db_url', value: db_url), string(name: 'db_tableName', value: db_tableName), string(name: 'artifact_version', value: artifact_version),[$class: 'NodeParameterValue', name: 'node_to_run', allNodesMatchingLabel: true,labels: [node_to_run], nodeEligibility: [$class: 'AllNodeEligibility'], triggerIfResult: 'allCases']]
         s
 }
 post
