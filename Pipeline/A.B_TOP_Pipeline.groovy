b85bbed749dbb501714be9f372e34fc0c6a56c3c
humphrey.lui <hum3lui@gmail.com>
2018-04-19 15:57:04 +0800

    TOP_Pipeline

---
+++ Pipeline/A.B_TOP_Pipeline.groovy
@@ -0,0 +1,65 @@
+pipeline {
+    agent none
+    parameters {
+        string(name: 'artifact_version', defaultValue: '0.1.0-SNAPSHOT', description: 'Artifact Built to JFrog')
+    }
+    stages {
+        stage('DEV') { 
+            input{ 
+                message 'param submission (by env_admin)'
+                submitter 'dev_admin,admin'
+                parameters{
+                    string(name: 'db_username', defaultValue: 'root', description: 'account for WebApp connect to MySQL')
+                    string(name: 'db_password', defaultValue: '')
+                }  
+            }
+            steps {
+                input message: 'Start DEV Deployment? (by approver)', submitter: 'admin,dev_admin'
+                build job: '/A.B/DEV/A.B_DEV_Pipeline', parameters: [string(name: 'artifact_version', value: artifact_version),string(name: 'db_username', value: db_username),string(name: 'db_password', value: db_password)]
+            }
+        }
+        stage('SIT') { 
+            input{ 
+                message 'param submission(by env_admin)'
+                submitter 'sit_admin,admin'
+                parameters{
+                    string(name: 'db_username', defaultValue: 'root', description: 'account for WebApp connect to MySQL')
+                    string(name: 'db_password', defaultValue: '',description: 'password of the account')
+                }  
+            }
+            steps {
+                input message: 'Start SIT Deployment?(by approver)'
+                build job:'/A.B/SIT/A.B_SIT_Pipeline', parameters: [string(name: 'artifact_version', value: artifact_version),string(name: 'db_username', value: db_username),string(name: 'db_password', value: db_password)]
+            }
+        }
+        stage('SAT') { 
+            input{ 
+                message 'param submission (by env_admin)'
+                submitter 'sat_admin,admin'
+                parameters{
+                    string(name: 'db_username', defaultValue: 'root', description: 'account for WebApp connect to MySQL')
+                    string(name: 'db_password', defaultValue: '',description: 'password of the account')
+                }  
+            }
+            steps {
+                input message: 'Start SAT Deployment? (by approver)'    
+                build job:'/A.B/SAT/A.B_SAT_Pipeline', parameters: [string(name: 'artifact_version', value: artifact_version),string(name: 'db_username', value: db_username),string(name: 'db_password', value: db_password)]
+            }
+        }
+        stage('PROD') { 
+            input{ 
+                message 'param submission(by env_admin)'
+                submitter 'sat_admin,admin'
+                parameters{
+                    string(name: 'db_username', defaultValue: 'root', description: 'account for WebApp connect to MySQL')
+                    string(name: 'db_password', defaultValue: '',description: 'password of the account')
+                }  
+            }
+            steps {
+                input message: 'Start PROD Deployment? (by approver)'   
+                build job:'/A.B/PROD/A.B_PROD_Pipeline', parameters: [string(name: 'artifact_version', value: artifact_version),string(name: 'db_username', value: db_username),string(name: 'db_password', value: db_password)]
+            }
+        }
+    }
+
+}
