f05f8b614e71e3adbef3cb338e60f75a0a86ce73
humphrey.lui <hum3lui@gmail.com>
2018-04-20 14:01:56 +0800

    pipeline1 added try/catch

--- deployWebAppPipeline.groovy
+++ deployWebAppPipeline.groovy
@@ -1,4 +1,6 @@
 node(node_to_run) {
+    stage('download'){
+    try{
     def server = Artifactory.server 'AWS-Artifactory'
     def downloadSpec = """{
         "files": [
@@ -9,10 +11,29 @@ node(node_to_run) {
         ]
     }"""
         server.download(downloadSpec);
-        build job: '/DSLteam1/DEV/job1', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'db_url', value: db_url), string(name: 'db_tableName', value: db_tableName), string(name: 'artifact_version', value: artifact_version),[$class: 'NodeParameterValue', name: 'node_to_run', allNodesMatchingLabel: true,labels: [node_to_run], nodeEligibility: [$class: 'AllNodeEligibility'], triggerIfResult: 'allCases']]
-        s
+        //build job: '/DSLteam1/DEV/job1', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'db_url', value: db_url), string(name: 'db_tableName', value: db_tableName), string(name: 'artifact_version', value: artifact_version),[$class: 'NodeParameterValue', name: 'node_to_run', allNodesMatchingLabel: true,labels: [node_to_run], nodeEligibility: [$class: 'AllNodeEligibility'], triggerIfResult: 'allCases']]
+        
+        if(currentBuild.result == null) {
+            currentBuild.result = "SUCCESS" // sets the ordinal as 4 and boolean to false
+        }
+    }
+    catch (err) {
+        if(currentBuild.result == null) {
+            currentBuild.result = "FAILURE" // sets the ordinal as 4 and boolean to false
+        }
+         throw err
+    }
+    }
+    stage('post'){
+                def mydata = [:]
+                mydata['pipeline'] = upstream
+                
+                step([$class: 'InfluxDbPublisher',
+                    customData: mydata,
+                    customDataMap: null,
+                    target: 'DSL_DB'])
+    }
 }
-post
-node(node_to_run){
-    echo 'influxDB task applys here'
-}
+
+
+
