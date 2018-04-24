4e621f649a1b002bdd58b9d887ad2ed2afe55918
humphrey.lui <hum3lui@gmail.com>
2018-04-24 13:33:16 +0800

    ENV_WebApp.Pipeline with influxDB & downstream build#

--- Pipeline/A.B_ENV_WebApp.Pipeline.groovy
+++ Pipeline/A.B_ENV_WebApp.Pipeline.groovy
@@ -5,11 +5,9 @@ pipeline {
             steps{
                 script{
                     try{
-                        build job: '/A.B/JOB/WebApp.DownloadArtifact', parameters: [string(name: 'artifact_version', value: artifact_version)]
-                    if(currentBuild.result == null) {
-                            currentBuild.result = "SUCCESS" // sets the ordinal as 0 and boolean to true
-                        }
-  
+                        def job1 = build job: '/A.B/JOB/WebApp.DownloadArtifact', parameters: [string(name: 'artifact_version', value: artifact_version)]
+                        job1_num = job1.getNumber()
+                        job1_result = job1.getResult()
                     }
                     catch (err) {
                         if(currentBuild.result == null) {
@@ -24,10 +22,9 @@ pipeline {
             steps{
                 script{
                     try{
-                        build job: '/A.B/JOB/WebApp.ReplaceToken', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'artifact_version', value: artifact_version)]
-                    if(currentBuild.result == null) {
-                            currentBuild.result = "SUCCESS" // sets the ordinal as 0 and boolean to true
-                        }
+                        def job2 = build job: '/A.B/JOB/WebApp.ReplaceToken', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'artifact_version', value: artifact_version)]
+                        job2_num = job2.getNumber()
+                        job2_result = job2.getResult()
   
                     }
                     catch (err) {
@@ -43,7 +40,10 @@ pipeline {
             steps{
                 script{
                     try{
-                         build job: '/A.B/JOB/WebApp.Deploy', parameters: [string(name: 'artifact_version', value: artifact_version), [$class: 'LabelParameterValue', name: 'node_to_run', label: node_to_run, allNodesMatchingLabel: true, nodeEligibility: [$class: 'AllNodeEligibility']]]
+                         def job3 = build job: '/A.B/JOB/WebApp.Deploy', parameters: [string(name: 'artifact_version', value: artifact_version), [$class: 'LabelParameterValue', name: 'node_to_run', label: node_to_run, allNodesMatchingLabel: true, nodeEligibility: [$class: 'AllNodeEligibility']]]
+                         job3_num = job3.getNumber()
+                         job3_result = job3.getResult()
+                         
                     if(currentBuild.result == null) {
                             currentBuild.result = "SUCCESS" // sets the ordinal as 0 and boolean to true
                         }
@@ -62,28 +62,54 @@ pipeline {
     }
     post {
             success{
-                step([$class: 'InfluxDbPublisher',
-                    customData: null,
-                    customDataMap: null,
-                    target: 'pipelineTestDB'])
+                script{
+                    def mydata = [:]
+                    mydata['pipeline'] = upstream
+                    mydata['job1_BuildNum'] = job1_num
+                    mydata['job1_result'] = job1_result
+                    mydata['job2_BuildNum'] = job2_num
+                    mydata['job2_result'] = job2_result
+                    mydata['job3_BuildNum'] = job3_num
+                    mydata['job3_result'] = job3_result
+                    step([$class: 'InfluxDbPublisher',
+                        customData: mydata,
+                        customDataMap: null,
+                        target: 'DSL_DB'])
+                }
             }
             aborted {
                 echo '<aborted message>'
                 script{
-                    currentBuild.result = "ABORTED"
+                    def mydata = [:]
+                    mydata['pipeline'] = upstream
+                    mydata['job1_BuildNum'] = job1_num
+                    mydata['job1_result'] = job1_result
+                    mydata['job2_BuildNum'] = job2_num
+                    mydata['job2_result'] = job2_result
+                    mydata['job3_BuildNum'] = job3_num
+                    mydata['job3_result'] = job3_result
                     step([$class: 'InfluxDbPublisher',
-                        customData: null,
+                        customData: mydata,
                         customDataMap: null,
-                        target: 'pipelineTestDB'])
+                        target: 'DSL_DB'])
                 }
             }
             failure {
                 echo '<rollback process>'
                 script{
+                    currentBuild.result = "ABORTED"
+                    def mydata = [:]
+                    mydata['pipeline'] = upstream
+                    mydata['job1_BuildNum'] = job1_num
+                    mydata['job1_result'] = job1_result
+                    mydata['job2_BuildNum'] = job2_num
+                    mydata['job2_result'] = job2_result
+                    mydata['job3_BuildNum'] = job3_num
+                    mydata['job3_result'] = job3_result
                     step([$class: 'InfluxDbPublisher',
-                        customData: null,
+                        customData: mydata,
                         customDataMap: null,
-                        target: 'pipelineTestDB'])
+                        target: 'DSL_DB'])
                 }
             }
     }
