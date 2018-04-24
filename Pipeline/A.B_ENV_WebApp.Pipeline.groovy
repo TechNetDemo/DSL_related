e9fcdbc3a2b4ab41d0aefb5987bd93459bd55190
humphrey.lui <hum3lui@gmail.com>
2018-04-24 17:45:35 +0800

    WebApp.Pipeline and DEV.Pipeline updated about influxDB

--- Pipeline/A.B_ENV_WebApp.Pipeline.groovy
+++ Pipeline/A.B_ENV_WebApp.Pipeline.groovy
@@ -4,57 +4,35 @@ pipeline {
         stage('Download'){
             steps{
                 script{
-                    try{
-                        def job1 = build job: '/A.B/JOB/WebApp.DownloadArtifact', parameters: [string(name: 'artifact_version', value: artifact_version)]
-                        job1_num = job1.getNumber()
-                        job1_result = job1.getResult()
-                    }
-                    catch (err) {
-                        if(currentBuild.result == null) {
-                            currentBuild.result = "FAILURE" // sets the ordinal as 4 and boolean to false
-                        }
-                        throw err
-                    }
+                        job1 = build job: '/A.B/JOB/WebApp.DownloadArtifact', parameters: [string(name: 'artifact_version', value: artifact_version)]
+                        mydata = [:]
+                        mydata['upstream'] = upstream
+                        mydata['job1_BuildNum'] = job1.getNumber()
+                        mydata['job1_Result'] = job1.getResult()
+
                 }
             }
         }
         stage('Replace'){
             steps{
                 script{
-                    try{
-                        def job2 = build job: '/A.B/JOB/WebApp.ReplaceToken', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'artifact_version', value: artifact_version)]
-                        job2_num = job2.getNumber()
-                        job2_result = job2.getResult()
-  
-                    }
-                    catch (err) {
-                        if(currentBuild.result == null) {
-                            currentBuild.result = "FAILURE" // sets the ordinal as 4 and boolean to false
-                        }
-                        throw err
-                    }
+
+                        job2 = build job: '/A.B/JOB/WebApp.ReplaceToken', parameters: [string(name: 'db_username', value: db_username), string(name: 'db_password', value: db_password), string(name: 'artifact_version', value: artifact_version)]
+                        mydata['job2_BuildNum'] = job2.getNumber()
+                        mydata['job2_Result'] = job2.getResult()
+
                 }
             }
         }
         stage('Deploy'){
             steps{
                 script{
-                    try{
-                         def job3 = build job: '/A.B/JOB/WebApp.Deploy', parameters: [string(name: 'artifact_version', value: artifact_version), [$class: 'LabelParameterValue', name: 'node_to_run', label: node_to_run, allNodesMatchingLabel: true, nodeEligibility: [$class: 'AllNodeEligibility']]]
-                         job3_num = job3.getNumber()
-                         job3_result = job3.getResult()
+
+                        job3 = build job: '/A.B/JOB/WebApp.Deploy', parameters: [string(name: 'artifact_version', value: artifact_version), [$class: 'LabelParameterValue', name: 'node_to_run', label: node_to_run, allNodesMatchingLabel: true, nodeEligibility: [$class: 'AllNodeEligibility']]]
+                        mydata['job3_BuildNum'] = job3.getNumber()
+                        mydata['job3_Result'] = job3.getResult()
                          
-                    if(currentBuild.result == null) {
-                            currentBuild.result = "SUCCESS" // sets the ordinal as 0 and boolean to true
-                        }
-  
-                    }
-                    catch (err) {
-                        if(currentBuild.result == null) {
-                            currentBuild.result = "FAILURE" // sets the ordinal as 4 and boolean to false
-                        }
-                        throw err
-                    }
+
                 }
             }
         }
@@ -63,14 +41,7 @@ pipeline {
     post {
             success{
                 script{
-                    def mydata = [:]
-                    mydata['pipeline'] = upstream
-                    mydata['job1_BuildNum'] = job1_num
-                    mydata['job1_result'] = job1_result
-                    mydata['job2_BuildNum'] = job2_num
-                    mydata['job2_result'] = job2_result
-                    mydata['job3_BuildNum'] = job3_num
-                    mydata['job3_result'] = job3_result
+                    currentBuild.result = "SUCCESS"
                     step([$class: 'InfluxDbPublisher',
                         customData: mydata,
                         customDataMap: null,
@@ -80,14 +51,7 @@ pipeline {
             aborted {
                 echo '<aborted message>'
                 script{
-                    def mydata = [:]
-                    mydata['pipeline'] = upstream
-                    mydata['job1_BuildNum'] = job1_num
-                    mydata['job1_result'] = job1_result
-                    mydata['job2_BuildNum'] = job2_num
-                    mydata['job2_result'] = job2_result
-                    mydata['job3_BuildNum'] = job3_num
-                    mydata['job3_result'] = job3_result
+                    currentBuild.result = "ABORTED"
                     step([$class: 'InfluxDbPublisher',
                         customData: mydata,
                         customDataMap: null,
@@ -97,15 +61,7 @@ pipeline {
             failure {
                 echo '<rollback process>'
                 script{
-                    currentBuild.result = "ABORTED"
-                    def mydata = [:]
-                    mydata['pipeline'] = upstream
-                    mydata['job1_BuildNum'] = job1_num
-                    mydata['job1_result'] = job1_result
-                    mydata['job2_BuildNum'] = job2_num
-                    mydata['job2_result'] = job2_result
-                    mydata['job3_BuildNum'] = job3_num
-                    mydata['job3_result'] = job3_result
+                    currentBuild.result = "FAILURE"
                     step([$class: 'InfluxDbPublisher',
                         customData: mydata,
                         customDataMap: null,
